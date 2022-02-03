1bd1e50a5da91107b4b606888d3b63c32180cd83
Mosholm <Oliv387u@stud.kea.dk>
2022-02-03 21:06:38 +0100

    fail branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+colors: {
+  // white: "#C5C9D6",
+  // secondary: "#5D5D81",
+  primary: "#4C486B",
+  accent:"454161",
+  // dark:"343149"
+}
+
+    extend: {}
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
