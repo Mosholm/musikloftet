a963c7468ba182d99259f426810931acaf0d8d10
Mosholm <Oliv387u@stud.kea.dk>
2022-02-21 11:40:06 +0100

    Screen sizes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-
-    colors:{
-      blue: '#183599',
-      orange: '#FBB539',
-      dark: '#000929',
-      light: '#F5F8FF'
+    colors: {
+      blue: "#183599",
+      orange: "#FBB539",
+      dark: "#000929",
+      light: "#F5F8FF",
+    },
+    screen: {
+      mobil: "960px",
+      desktop: "1248px",
     },
 
-    fontsize:{
-      header: ['144px', {lineHeight: '170px'}]
+    fontsize: {
+      header: ["144px", { lineHeight: "170px" }],
     },
 
     fontFamily: {
-      Futura: [futura-pt, sans-serif],
-      
-    }
-
-    extend: {
+      Futura: [futura - pt, sans - serif],
     },
+
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
